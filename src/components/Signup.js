e9e7992c9886ab40ff63168937ee46fbeb2ce752
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-12-05 15:24:48 +0300

    add sign up form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,21 +1,32 @@
-import React, {useState}from "react";
-impo
+import React, { useState } from "react";
 
-function Signup()
-const [username, setUsername]= useState("")
-const [password, setPassword] = useState("")
-const [passwordConfirmation, setPasswordConfirmation] = useState("");
-const [errors, setErrors] = useState([]);
+export const Signup = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
 
+    const [name, setName] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
 
-return(
-  <>
-  <form>
-    <label for="username">Username</label>
-  <input value={username} ></input>
-  </form>
-  </>
-)
-
-export default Signup
+    return (
+        <div className="auth-form-container">
+            <h2>Signup</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+            <label htmlFor="name">Full name</label>
+            <input value={name} onChange={(e)=> setName(e.target.value)} type="text" placeholder="Your Name" id="name" name="name"/>
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">password</label>
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <label htmlFor="password">Password Confirmation</label>
+        <input type="password" id="password_confirmation" value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)}/> 
+            <button type="submit">Log In</button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+    </div>
+    )
+}
