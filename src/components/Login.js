02688d315db635104205907ce87108e1ce6ac215
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-12-06 16:05:30 +0300

    add function and fetch data from backend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,59 @@
 import React, { useState } from "react";
+import { json } from "react-router-dom";
 
 export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassward] = useState('');
-  
+  const [username, setUsername] = useState(null);
+  const [password, setPassward] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password })
+    })
+    .then((res) => {
+        if (res.ok) {
+          res.json().then((data) => console.log(data));
+        }
+        else {
+            res.json().then(err=>console.log(err))
+        }
+      })
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email)
-        console.log(password);
-    }
-
-    return (
-        <div className="auth-form-container">
-            <h2>Login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={password} onChange={(e) => setPassward(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-        </div>
-    )
-}
+  return (
+    <div className="auth-form-container">
+      <h2>Login</h2>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <label htmlFor="username">username</label>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="name"
+          placeholder="Your name"
+          id="name"
+          name="name"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          value={password}
+          onChange={(e) => setPassward(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        <button type="submit">Log In</button>
+      </form>
+      <button
+        className="link-btn"
+        onClick={() => props.onFormSwitch("register")}
+      >
+        Don't have an account? Register here.
+      </button>
+    </div>
+  );
+};
