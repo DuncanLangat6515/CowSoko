9702d0cdc2638da1d8411cf94b9a901908fd5c68
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-12-06 16:10:48 +0300

    add component newcow post data to backend

---
+++ src/components/NewCow.js
@@ -0,0 +1,53 @@
+import React,{useState} from "react";
+
+function NewCow() {
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [price, setPrice] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/cows", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: name,
+            image: image,
+            price: price,
+          }),
+        });
+      }
+      return (
+        <div className="new-cow-form">
+          <h2>New Cow</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Cow name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="text"
+              name="image"
+              placeholder="Image URL"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+            />
+            <input
+              type="number"
+              name="price"
+              placeholder="Price"
+              value={price}
+              onChange={(e) => setPrice(parseFloat(e.target.value))}
+            />
+            <button type="submit">Post Cow</button>
+          </form>
+        </div>
+      );
+}
+
+export default NewCow
