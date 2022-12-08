5ac5ede14296cb2aa2c4ea934ac99a71348ce0fe
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-12-08 08:15:31 +0300

    add function,fetch data and make changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import {Signup}  from "./components/Signup";
@@ -6,15 +5,42 @@ import Navbar from "./components/Navbar";
 import  {Login}  from "./components/Login";
 import Home from "./components/Home"
 import NewCow from "./components/NewCow";
+import React, { useEffect, useState } from 'react'
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
+  const[cows, setCows]=useState([])
+  const [newData, setNewData] = useState(false);
+
+  useEffect(()=>{
+      fetch('/cows')
+      .then(r=>r.json())
+      .then(data=>{
+          setCows(data)
+      })
+  },[newData])
+  
+  function handleDelete(id){
+    console.log(id)
+    fetch(`/cows/${id}`,{
+      method: "DELETE",
+    })
+    .then(res=>res.json())
+    .then(()=>{
+      const remainingCows=cows.filter((remaining)=>remaining.id!==id)
+      setCows(remainingCows)
+    })
+    handleNewData()
+  }
+  function handleNewData() {
+    setNewData((newData) => !newData);
+  }
 
   return (
     <div className="App">
       <Navbar/>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/" element={<Home cows={cows} handleDelete={handleDelete}/>}/>
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/signup" element={<Signup/>}/>
         {/* <Route exact path="/logout" element={<Logout/>}/> */}
