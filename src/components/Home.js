8069cc6e4fcc0f5bc48d383591a671c99dc97130
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-12-09 14:45:23 +0300

    update with prompt

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 
 function Home({cows, handleDelete}) {
 
 
-
+function handleUpdate(id){
+let price = parseInt(prompt('Price input'));
+  fetch(`/cows/${id}`,{
+    method:'PATCH',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify(price)
+  })
+ 
+}
     const cowPic=cows.map((cow)=>(
         <div className='cow_card'>
         <img src={cow.image}/>
         <h2>Breeds: {cow.name}</h2>
         <p><span>Ksh:</span>{cow.price}</p>
-        <button>update price</button>
+        <button onClick={()=>handleUpdate(cow.id)}>update price</button>
         <button onClick={()=>handleDelete(cow.id)}>Purchase</button>
         </div>
     ))
