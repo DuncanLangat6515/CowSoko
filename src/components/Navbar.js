76e2ab37d791fbce95fa4abd18b850e95c0d99d7
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-12-09 12:36:05 +0300

    comment link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <div className='navbar'>
-      <Link to="/">Home</Link>
+      {/* <Link to="/">Home</Link> */}
       <Link to="/login">Login</Link>
       <Link to="/signup">Signup</Link>
-      <Link to="/addcow">Add Cow</Link>
+      {/* <Link to="/addcow">Add Cow</Link> */}
     </div>
   )
 }
